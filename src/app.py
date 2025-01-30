0d0a84189f7b2e0a278db8e3da8d354681e05ba5
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2025-01-30 23:14:09 +0000

    primer commit

--- src/app.py
+++ src/app.py
@@ -15,6 +15,8 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -25,18 +27,56 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+##### Realiza GET total####
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+  
+    return jsonify(members), 200
+
+##### Realiza Get individual####
+@app.route('/member/<int:id>', methods =['GET'])
+def get_single_member(id):
+    
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+   
+    return jsonify({"error": "Miembro en la familia no encontrado"}), 404
+   
+##### Realiza POST ####
+@app.route('/member', methods =['POST'])
+def add_miembro():
+    member = request.json
+    if not member:
+        return jsonify({"error": "No se pudo añadir ningun mienbro"}), 400
+    
+    jackson_family.add_member(member)
+    
+    return jsonify({"mensaje": "Un nuevo miembro añadido a la familia"}), 200
+    
+  
+
+##### Realiza DELETE ####  
+@app.route('/member/<int:id>', methods =['DELETE'])
+def delete_single_member(id):
+   
+    member =jackson_family.get_member(id)
+    
+    if member:
+        jackson_family.delete_member(id)
+
+        return jsonify({"mensaje": "Miembro eliminado correctamente"}), 200
+    
+    return jsonify({"error": "No se pudo eliminar el miembro"}), 400
+   
+   
+        
 
 
-    return jsonify(response_body), 200
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
